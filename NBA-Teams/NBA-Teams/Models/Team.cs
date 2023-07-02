8465d2dc1412bfb4c32ad104b783c32a8d4a6dab
FrosinaStamenkovska <117694114+FrosinaStamenkovska@users.noreply.github.com>
2023-07-02 16:58:40 +0200

    Delete NBA-Teams directory

--- NBA-Teams/NBA-Teams/Models/Team.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NBA_Teams.Models
-{
-    public class Team
-    {
-        public int Id { get; set; }
-        public string Abbreviation { get; set; }
-        public string Name { get; set; }
-    }
-}
